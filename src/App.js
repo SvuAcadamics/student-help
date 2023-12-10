43aa2156a2c3f19d3158166e1ae8c30b245548f1
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2023-12-10 20:52:25 +0530

    Introduced dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Questionpaper from './pages/Questionpapers/Questionpaper';
@@ -7,14 +7,20 @@ import Syllabus from './pages/Syllabus/Syllabus';
 import AboutUs from './pages/AboutUs/AboutUs';
 
 const App = () => {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
   return (
     <Router>
-      <Navigation />
+      <Navigation theme={theme} toggleTheme={toggleTheme} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/syllabus" element={<Syllabus />} />
-        <Route path="/questionpaper" element={<Questionpaper />} />
-        <Route path='/AboutUs' element={<AboutUs/>}/>
+        <Route path="/" element={<Home theme={theme} />} />
+        <Route path="/syllabus" element={<Syllabus theme={theme} />} />
+        <Route path="/questionpaper" element={<Questionpaper theme={theme} />} />
+        <Route path='/AboutUs' element={<AboutUs theme={theme} />} />
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+// index.js or App.js
+
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Function to toggle the theme
+const toggleTheme = () => {
+  const rootElement = document.documentElement;
+  rootElement.classList.toggle('dark');
+  rootElement.classList.toggle('light');
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const AppWithThemeToggle = () => {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleAndSetTheme = () => {
+    toggleTheme();
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <React.StrictMode>
+      <App theme={theme} toggleTheme={toggleAndSetTheme} />
+    </React.StrictMode>
+  );
+};
+
+root.render(<AppWithThemeToggle />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
