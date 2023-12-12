916d0978930ef26d28b7745fdbd95b7518801bb0
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2023-12-12 18:28:55 +0530

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,18 @@ import Questionpaper from './pages/Questionpapers/Questionpaper';
 import Navigation from './components/Navigator/Navigation';
 import Syllabus from './pages/Syllabus/Syllabus';
 import AboutUs from './pages/AboutUs/AboutUs';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-  const [theme, setTheme] = useState('dark');
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
-  };
-
+  
   return (
     <Router>
-      <Navigation theme={theme} toggleTheme={toggleTheme} />
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home theme={theme} />} />
-        <Route path="/syllabus" element={<Syllabus theme={theme} />} />
-        <Route path="/questionpaper" element={<Questionpaper theme={theme} />} />
-        <Route path='/AboutUs' element={<AboutUs theme={theme} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/syllabus" element={<Syllabus />} />
+        <Route path="/questionpaper" element={<Questionpaper />} />
+        <Route path='/AboutUs' element={<AboutUs/>} />
       </Routes>
     </Router>
   );
