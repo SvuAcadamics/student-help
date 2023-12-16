13afaa96dda389b881fff973df1a89c9ff3267ce
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2023-12-16 21:24:00 +0530

    Changed to sync

--- src/App.js
+++ src/App.js
@@ -5,29 +5,11 @@ import Questionpaper from './pages/Questionpapers/Questionpaper';
 import Syllabus from './pages/Syllabus/Syllabus';
 import AboutUs from './pages/AboutUs/AboutUs';
 import Navbar from './components/Navbar/Navbar';
-<<<<<<< HEAD
-import Download from './components/Download/Download';
-=======
 import './App.css';
->>>>>>> 1e738f5bf9402e5c4239a5738a2a9a6d7ce3ced1
 
 const App = () => {
   
   return (
-<<<<<<< HEAD
-    <>
-      <Download/>
-    </>
-    // <Router>
-    //   <Navbar/>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/syllabus" element={<Syllabus />} />
-    //     <Route path="/questionpaper" element={<Questionpaper />} />
-    //     <Route path='/AboutUs' element={<AboutUs/>} />
-    //   </Routes>
-    // </Router>
-=======
     <Router>
       <Navbar/>
       <Routes>
@@ -37,7 +19,6 @@ const App = () => {
         <Route path='/AboutUs' element={<AboutUs/>} />
       </Routes>
     </Router>
->>>>>>> 1e738f5bf9402e5c4239a5738a2a9a6d7ce3ced1
   );
 };
 
