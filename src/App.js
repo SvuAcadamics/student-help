9ea3ffcff195330db2a2526abf02a570d91144ee
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2024-02-20 23:12:26 +0530

    Started from scratch again just to improve ui

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-// import React, { lazy, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import { Route as RouteNames, RoutePrefix } from './roots';
-import LazyLoadComp from './lazyLoadComp';
+import React from 'react'
 
 const App = () => {
-
   return (
-    <Router>
-      <Navbar/>
-      <main>
-      <Routes>
-        <Route path='/' element={<LazyLoadComp filePath='./pages/Home/Home'/>} />
-        <Route path={RoutePrefix} element={<LazyLoadComp filePath='./pages/Home/Home'/>} />
-        <Route path={`${RoutePrefix}/${RouteNames.Home}`} element={<LazyLoadComp filePath='./pages/Home/Home'/>}/>
-        <Route path={`${RoutePrefix}/${RouteNames.Syllabus}`} element={<LazyLoadComp filePath='./pages/Syllabus/Syllabus'/>} />
-        <Route path={`${RoutePrefix}/${RouteNames.QuestionPaper}`} element={<LazyLoadComp filePath='./pages/Questionpapers/Questionpaper'/>} />
-        <Route path={`${RoutePrefix}/${RouteNames.Aboutus}`} element={<LazyLoadComp filePath='./pages/AboutUs/AboutUs'/>} />
-        <Route path='/login' element={<LazyLoadComp filePath='./containers/LoginContainer'/>} />  
-        <Route path='/registration' element={<LazyLoadComp filePath='./containers/RegistrationContainer' />} />             
-      </Routes>
-      </main>
-      <Footer/>
-    </Router>
+    <div>
+      hello
+    </div>
+  )
+}
 
-  );
-};
-
-export default App;
+export default App
